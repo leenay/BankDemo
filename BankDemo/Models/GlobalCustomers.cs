c2455660cf7f7b6e4b8909381344844c6f6a6726
Lee Naylor <Lee.Naylor@open-money.co.uk>
2020-02-27 21:05:57 +0000

    added globalcustomers model

---
+++ BankDemo/Models/GlobalCustomers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankDemo.Models
+{
+    public static class GlobalCustomers
+    {
+        static GlobalCustomers()
+        {
+            Customers = new List<Customer> {
+                new Customer { Id = 1, Name = "Customer1" },
+                new Customer { Id = 2, Name = "Customer2" }
+            };
+        }
+        public static List<Customer> Customers { get; set; }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
